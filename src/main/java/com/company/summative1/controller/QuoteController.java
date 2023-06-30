84da4923a2688eaa1945f653a9b6117e4aaa4238
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2023-06-29 20:59:07 -0400

    Created Quote class

--- src/main/java/com/company/summative1/controller/QuoteController.java
+++ src/main/java/com/company/summative1/controller/QuoteController.java
@@ -1,4 +1,20 @@
 package com.company.summative1.controller;
 
+import com.company.summative1.model.Quote;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+@RestController
 public class QuoteController {
+    private static int idCounter = 1;
+    private List<Quote> quotelist;
+    public QuoteController(){
+        Quote quote1 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
+        quotelist = Arrays.asList(quote1);
+    }
+
+
 }
--- src/main/java/com/company/summative1/model/Quote.java
+++ src/main/java/com/company/summative1/model/Quote.java
@@ -1,4 +1,38 @@
 package com.company.summative1.model;
 
 public class Quote {
+    int id;
+    String author;
+    String quote;
+
+    public Quote(int id, String author, String quote){
+        this.id = id;
+        this.author = author;
+        this.quote = quote;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
 }

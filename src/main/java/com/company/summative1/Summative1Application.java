618dadfd125f86a06d3652f4f2f2eec119808a47
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2023-06-29 20:18:47 -0400

    Cloned Repo

---
+++ src/main/java/com/company/summative1/Summative1Application.java
@@ -0,0 +1,13 @@
+package com.company.summative1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/summative1/Summative1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.summative1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

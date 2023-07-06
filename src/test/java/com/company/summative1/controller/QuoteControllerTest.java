807453abcee0ec2bafa4e3c46b331d24384c707b
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2023-07-06 18:12:23 -0400

    Completed QuoteControllerTest

---
+++ src/test/java/com/company/summative1/controller/QuoteControllerTest.java
@@ -0,0 +1,34 @@
+package com.company.summative1.controller;
+import com.company.summative1.model.Quote;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(QuoteController.class)
+class QuoteControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<Quote> quoteList;
+
+    @Test
+    void shouldGetRandomQuote() throws Exception {
+        String output = objectMapper.writeValueAsString(quoteList);
+
+        mockMvc.perform(get("/quote"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}

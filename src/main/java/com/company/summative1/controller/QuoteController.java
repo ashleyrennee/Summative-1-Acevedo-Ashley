47dc42ce6d150686c54d3948a083376d482ab54d
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2023-07-06 12:00:37 -0400

    Created quotes

--- src/main/java/com/company/summative1/controller/QuoteController.java
+++ src/main/java/com/company/summative1/controller/QuoteController.java
@@ -15,19 +15,21 @@ public class QuoteController {
     private static int idCounter = 1;
     private List<Quote> quotelist;
     public QuoteController(){
-        Quote quote1 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote2 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote3 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote4 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote5 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote6 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote7 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote8 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote9 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote10 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote11 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        Quote quote12 = new Quote(idCounter++,"Ashley","2 b or not 2 b");
-        quotelist = Arrays.asList(quote1,quote2,quote3,quote4,quote5,quote6,quote7,quote8,quote9,quote10,quote11,quote12);
+        Quote quote1 = new Quote(idCounter++,"Audre Lorde","My silences had not protected me.\n" +
+                " Your silence will not protect you.");
+        Quote quote2 = new Quote(idCounter++,"Angela Y. Davis","It is in collectivities that we find " +
+                "reservoirs of hope and optimism.");
+        Quote quote3 = new Quote(idCounter++,"Robin Wall Kimmerer","The land knows you even when you are lost");
+        Quote quote4 = new Quote(idCounter++,"Bell Hooks","Love is an action, never simply a feeling");
+        Quote quote5 = new Quote(idCounter++,"Carmen Maria Machado","We deserve to have our wrongdoing " +
+                "represented as much as our heroism, because when we refuse wrongdoing as a possibility for a group of people, we refuse their humanity.");
+        Quote quote6 = new Quote(idCounter++,"Carmen Maria Machado","Fear makes liars of us all");
+        Quote quote7 = new Quote(idCounter++,"Stephen Graham Jones","We all become history at some point, right?");
+        Quote quote8 = new Quote(idCounter++,"Elizabeth Acevedo","And I think about all the things we could be " +
+                "if we were never told our bodies were not built for them.");
+        Quote quote9 = new Quote(idCounter++,"Wally Lamb","But what are our stories if not the mirrors we hold up to our fears?");
+        Quote quote10 = new Quote(idCounter++,"Bell Hooks","Being oppressed means the absence of choices");
+        quotelist = Arrays.asList(quote1,quote2,quote3,quote4,quote5,quote6,quote7,quote8,quote9,quote10);
     }
 
     @RequestMapping(value = "/quote", method = RequestMethod.GET)
